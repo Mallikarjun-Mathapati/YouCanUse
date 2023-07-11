491ede5509bd0d502ec838290ede6da7a48bc636
Mallu0007 <Mallikarjun000007@gmail.com>
2023-07-11 13:18:00 +0530

    pages

--- src/Pages/Timer.js
+++ src/Pages/Timer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import LittlePro from '../Sections/LittlePro';
 import Practice from '../Sections/Practice';
 import ClickBelow from '../Sections/ClickBelow';
@@ -6,6 +6,33 @@ import AboutMe from '../Sections/AboutMe';
 
 
 const Timer = () => {
+  const [sec, setSec] = useState (0)
+  const [min, setMin] = useState (0)
+  const [hor, setHor   ] = useState (0)
+
+ var timer
+  useEffect(() => {
+    timer = setInterval(()=> {
+      setSec (sec + 1 )
+      if (sec === 59){
+        setSec(0)
+        setMin(min + 1)
+      }
+      if (min === 59) {
+        setHor(hor + 1)
+        setMin(0)
+      }
+    },1000)
+    return ()=> clearInterval(timer)
+  })
+  const StopBtn = () => {
+    clearInterval(timer)
+  }
+  const ResetBtn = () => {
+    setSec(0)
+    setMin(0)
+    setHor(0)
+  }
   return (
     <>
     <div className="timer-heading">
@@ -16,11 +43,11 @@ const Timer = () => {
             <div className="container">
                 <div className="timer-sub-main">
                     <div className="timer-count">
-                    <h2>00:00:00</h2>
+                    <h2>{hor < 10 ? '0' + hor : ''}:{min < 10 ? '0' + min : min}:{sec < 10 ? '0' + sec : sec}</h2>
                     </div>
                     <div className="timer-count">
-                    <button>Stop</button>
-                    <button>Reset</button>
+                    <button onClick={StopBtn}>Stop</button>
+                    <button onClick={ResetBtn}>Reset</button>
                     </div>
                 </div>
             </div>
